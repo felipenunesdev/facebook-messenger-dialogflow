25da459fd9916198b9fdd7df0fbb7ecd0302a836
Felipe Nunes <fdesouza@mobilecomputing.com.ar>
2020-09-22 18:30:38 -0300

    Fix webhook

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -4,7 +4,7 @@ import { Controller, Get, HttpStatus, Req, Res } from '@nestjs/common';
 export class WebhookController {
 
     @Get("/")
-    verifyToken(@Res() res: any, @Req() req: any){
+    verifyToken(@Req() req: any, @Res() res: any){
         console.log(res);
         console.log(req);
         return res.status(HttpStatus.OK).json(req);
--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,4 +1,10 @@
+import { WebhookController } from './webhook.controller';
 import { Module } from '@nestjs/common';
+import { WebhookService } from './webhook.service';
 
-@Module({})
+@Module({
+    imports: [],
+    controllers: [WebhookController],
+    providers: [WebhookService],
+})
 export class WebhookModule {}
