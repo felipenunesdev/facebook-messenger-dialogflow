c8d64c9c6329b170c5c10c7b0ed6e84d34c3c60c
Felipe Nunes <fdesouza@mobilecomputing.com.ar>
2020-09-22 17:38:55 -0300

    Test controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post("/messages")
+  getMessages(@Res() response: any, @Body() body: any): string {
+    console.log(body)
+    console.log(response)
     return this.appService.getHello();
   }
 }
